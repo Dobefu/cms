3080d9348de13e2aeb54ea5c5205668763719f23
Connor van Spronssen <info@dobefu.com>
2025-03-03 14:02:24 +0100

    feat: Add some standard Next configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole:
+      process.env.NODE_ENV === 'production'
+        ? {
+            exclude: ['warn', 'error'],
+          }
+        : false,
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
