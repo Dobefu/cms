dd4b88d4329e58fe79af0470e84aeab2ca1f4c9c
Connor van Spronssen <info@dobefu.com>
2025-03-08 08:58:56 +0100

    test: Add a test to the header component

---
+++ src/components/layout/header.test.tsx
@@ -0,0 +1,15 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it } from 'vitest'
+import Header from './header'
+
+describe('Header', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('Renders normally', () => {
+    render(<Header />)
+
+    expect(screen.getByRole('banner')).toBeDefined()
+  })
+})
