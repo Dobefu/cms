fe09bd4667fb4f1613abb22409974b0085431c67
Connor van Spronssen <info@dobefu.com>
2025-03-08 12:15:09 +0100

    fix: Remove the border from button input elements

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -11,12 +11,16 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
         typeClasses =
           'text-white bg-blue-600 dark:bg-blue-700 dark:not-disabled:hover:bg-blue-600 font-medium not-disabled:hover:bg-blue-700 outline-offset-4 not-disabled:cursor-pointer transition-colors dark:not-disabled:active:bg-blue-800 not-disabled:active:bg-blue-800'
         break
+      default:
+        typeClasses =
+          'dark:border-zinc-600 dark:bg-zinc-950 border border-zinc-200'
+        break
     }
 
     return (
       <input
         className={cn(
-          'w-full rounded-lg border border-zinc-200 px-3 py-2.5 shadow-xs disabled:opacity-75 dark:border-zinc-600 dark:bg-zinc-950',
+          'rounded-lg px-3 py-2.5 shadow-xs disabled:opacity-75',
           typeClasses,
           className,
         )}
