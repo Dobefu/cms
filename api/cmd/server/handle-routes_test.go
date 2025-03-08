5522104d391be43cabb2f7bd50598ae844891c48
Connor van Spronssen <info@dobefu.com>
2025-03-08 12:30:48 +0100

    test: Add a test to the HandleRoutes function

--- api/cmd/server/handle-routes_test.go
+++ api/cmd/server/handle-routes_test.go
@@ -1,15 +1,19 @@
 package server
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	routes_v1 "github.com/Dobefu/cms/api/cmd/server/routes/v1"
 	"github.com/stretchr/testify/assert"
 )
 
-func setupHandleRoutesTests(t *testing.T, method string, path string, body io.Reader) (rr *httptest.ResponseRecorder) {
+func setupHandleRoutesTests(t *testing.T, method string, path string, body io.Reader) (rr *httptest.ResponseRecorder, cleanup func()) {
+	routesV1Login = func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, "{}") }
+
 	mux := http.NewServeMux()
 	handleRoutes(mux)
 
@@ -19,20 +23,35 @@ func setupHandleRoutesTests(t *testing.T, method string, path string, body io.Re
 	rr = httptest.NewRecorder()
 	mux.ServeHTTP(rr, req)
 
-	return rr
+	return rr, func() {
+		routesV1Login = routes_v1.Login
+	}
 }
 
 func TestHandleRoutesHomepage(t *testing.T) {
-	rr := setupHandleRoutesTests(t, "GET", "/", nil)
+	rr, cleanup := setupHandleRoutesTests(t, "GET", "/", nil)
+	defer cleanup()
+
 	assert.Equal(t, http.StatusNotFound, rr.Code)
 }
 
 func TestHandleRoutesTrailingSlash(t *testing.T) {
-	rr := setupHandleRoutesTests(t, "GET", "/api/", nil)
+	rr, cleanup := setupHandleRoutesTests(t, "GET", "/api/", nil)
+	defer cleanup()
+
 	assert.Equal(t, http.StatusSeeOther, rr.Code)
 }
 
-func TestHandleRoutesApi(t *testing.T) {
-	rr := setupHandleRoutesTests(t, "GET", "/api/v1", nil)
+func TestHandleRoutesApiV1(t *testing.T) {
+	rr, cleanup := setupHandleRoutesTests(t, "GET", "/api/v1", nil)
+	defer cleanup()
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestHandleRoutesApiV1Login(t *testing.T) {
+	rr, cleanup := setupHandleRoutesTests(t, "GET", "/api/v1/login", nil)
+	defer cleanup()
+
 	assert.Equal(t, http.StatusOK, rr.Code)
 }
