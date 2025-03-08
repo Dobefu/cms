1f86d8c36bff29bc0d53e272a7696c83b89ebc4a
Connor van Spronssen <info@dobefu.com>
2025-03-08 08:52:19 +0100

    feat: Add a header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/layout/header'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -31,6 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-zinc-100 text-black antialiased dark:bg-zinc-900 dark:text-white`}
       >
+        <Header />
+
         {children}
       </body>
     </html>
