8155c670144b91e997b664a8526b96771d5f50c6
Connor van Spronssen <info@dobefu.com>
2025-03-08 09:21:59 +0100

    feat: Add some styling to the header

--- src/components/layout/header.test.tsx
+++ src/components/layout/header.test.tsx
@@ -11,5 +11,6 @@ describe('Header', () => {
     render(<Header />)
 
     expect(screen.getByRole('banner')).toBeDefined()
+    expect(screen.getByRole('navigation')).toBeDefined()
   })
 })
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,34 @@
+import Link from 'next/link'
+
 export default function Header() {
-  return <header>HEADER</header>
+  const links = [
+    {
+      title: 'Home',
+      href: '/',
+    },
+    {
+      title: '404',
+      href: '/2',
+    },
+  ]
+
+  return (
+    <header className="sticky top-0 p-4">
+      <nav aria-label="Navigation menu" className="flex items-center gap-4">
+        <div className="flex-1 text-2xl font-medium">
+          <Link aria-label="Homepage" href="/">
+            {process.env.APP_NAME}
+          </Link>
+        </div>
+
+        <div className="flex gap-4">
+          {links.map((link) => (
+            <a className="py-2 font-medium" href={link.href} key={link.href}>
+              {link.title}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
 }
