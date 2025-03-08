8674ac4e9e95259be82a21e53aa974fe0a87a644
Connor van Spronssen <info@dobefu.com>
2025-03-08 09:34:58 +0100

    feat: Add a custom not found template

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col gap-2 px-4">
+      <h1 className="text-2xl font-medium">404</h1>
+      <p>This page could not be found.</p>
+    </div>
+  )
+}
