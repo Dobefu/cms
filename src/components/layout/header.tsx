ef63ff2b0de78044f29eb8f897a30a718b74a6ed
Connor van Spronssen <info@dobefu.com>
2025-03-08 10:21:07 +0100

    fix: Resolve a11y issues in the header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
       href: '/',
     },
     {
-      title: '404',
-      href: '/2',
+      title: 'Login',
+      href: '/login',
     },
   ]
 
@@ -16,19 +16,14 @@ export default function Header() {
     <header className="sticky top-0 p-4">
       <nav aria-label="Navigation menu" className="flex items-center gap-4">
         <div className="flex-1 text-2xl font-medium">
-          <Link aria-label="Homepage" href="/" role="navigation">
+          <Link aria-label="Homepage" href="/">
             {process.env.APP_NAME}
           </Link>
         </div>
 
         <div className="flex gap-4">
           {links.map((link) => (
-            <Link
-              className="py-2 font-medium"
-              href={link.href}
-              key={link.href}
-              role="navigation"
-            >
+            <Link className="py-2 font-medium" href={link.href} key={link.href}>
               {link.title}
             </Link>
           ))}
