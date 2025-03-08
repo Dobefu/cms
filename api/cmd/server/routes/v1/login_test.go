1e42ed47d2433d8db54c6b64644ffbcdbcae5839
Connor van Spronssen <info@dobefu.com>
2025-03-08 12:35:50 +0100

    test: Add a test to the Login route function

---
+++ api/cmd/server/routes/v1/login_test.go
@@ -0,0 +1,26 @@
+package v1
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func setupLoginTests() (rr *httptest.ResponseRecorder, cleanup func()) {
+	rr = httptest.NewRecorder()
+
+	return rr, func() {}
+}
+
+func TestLogin(t *testing.T) {
+	rr, cleanup := setupLoginTests()
+	defer cleanup()
+
+	req, err := http.NewRequest("GET", "", nil)
+	assert.NoError(t, err)
+
+	Login(rr, req)
+	assert.JSONEq(t, "{}", rr.Body.String())
+}
