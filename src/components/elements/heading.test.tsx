90e12e34153165babd6b6a55a131c40eb64d76dc
Connor van Spronssen <info@dobefu.com>
2025-03-08 10:40:47 +0100

    feat: Make the headings a bit smaller

--- src/components/elements/heading.test.tsx
+++ src/components/elements/heading.test.tsx
@@ -10,37 +10,37 @@ describe('Heading', () => {
   it('Renders normally (h1)', () => {
     render(<Heading level={1}>TITLE</Heading>)
 
-    expect(screen.getByRole('heading').className).toContain('text-4xl')
+    expect(screen.getByRole('heading').className).toContain('text-3xl')
   })
 
   it('Renders normally (h2)', () => {
     render(<Heading level={2}>TITLE</Heading>)
 
-    expect(screen.getByRole('heading').className).toContain('text-3xl')
+    expect(screen.getByRole('heading').className).toContain('text-2xl')
   })
 
   it('Renders normally (h3)', () => {
     render(<Heading level={3}>TITLE</Heading>)
 
-    expect(screen.getByRole('heading').className).toContain('text-2xl')
+    expect(screen.getByRole('heading').className).toContain('text-xl')
   })
 
   it('Renders normally (h4)', () => {
     render(<Heading level={4}>TITLE</Heading>)
 
-    expect(screen.getByRole('heading').className).toContain('text-xl')
+    expect(screen.getByRole('heading').className).toContain('text-lg')
   })
 
   it('Renders normally (h5)', () => {
     render(<Heading level={5}>TITLE</Heading>)
 
-    expect(screen.getByRole('heading').className).toContain('text-lg')
+    expect(screen.getByRole('heading').className).toContain('text-md')
   })
 
   it('Renders normally (h6)', () => {
     render(<Heading level={6}>TITLE</Heading>)
 
-    expect(screen.getByRole('heading').className).toContain('text-md')
+    expect(screen.getByRole('heading').className).toContain('text-sm')
   })
 
   it('Does not render on invalid levels', () => {
--- src/components/elements/heading.tsx
+++ src/components/elements/heading.tsx
@@ -18,24 +18,34 @@ export default function Heading({ children, className, level }: Props) {
 
   switch (level) {
     case 1:
-      classes = 'text-4xl'
-      break
-    case 2:
       classes = 'text-3xl'
       break
-    case 3:
+    case 2:
       classes = 'text-2xl'
       break
-    case 4:
+    case 3:
       classes = 'text-xl'
       break
-    case 5:
+    case 4:
       classes = 'text-lg'
       break
-    case 6:
+    case 5:
       classes = 'text-md'
+      break
+    case 6:
+      classes = 'text-sm'
       break
   }
 
-  return <Tag className={cn('font-medium', classes, className)}>{children}</Tag>
+  return (
+    <Tag
+      className={cn(
+        'font-medium text-gray-800 dark:text-white',
+        classes,
+        className,
+      )}
+    >
+      {children}
+    </Tag>
+  )
 }
