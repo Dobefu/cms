e4390220e0f2454207c2d71e1c9b89ba9335813b
Connor van Spronssen <info@dobefu.com>
2025-03-09 15:11:12 +0100

    feat: Add logic to redirect to the /user page when authenticated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,22 @@
 import Heading from '@/components/elements/heading'
 import LoginForm from '@/components/forms/login.client'
 import Container from '@/components/layout/container'
+import { validateSession } from '@/utils/validate-session'
 import { Metadata } from 'next'
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
   title: 'Log in',
 }
 
-export default function Login() {
+export default async function Login() {
+  const { isAnonymous } = await validateSession()
+
+  if (!isAnonymous) {
+    redirect('/user')
+  }
+
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <Container className="flex max-w-xl flex-col gap-8">
