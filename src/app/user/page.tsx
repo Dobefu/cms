b53ed2bd7a7ce6a3e8c5c2bd5ef86499d39549a9
Connor van Spronssen <info@dobefu.com>
2025-03-09 17:52:39 +0100

    feat: Add a user page

---
+++ src/app/user/page.tsx
@@ -0,0 +1,22 @@
+import Heading from '@/components/elements/heading'
+import { validateSession } from '@/utils/validate-session'
+import { Metadata } from 'next'
+import { redirect } from 'next/navigation'
+
+export const metadata: Metadata = {
+  title: 'My account',
+}
+
+export default async function Login() {
+  const { isAnonymous } = await validateSession()
+
+  if (isAnonymous) {
+    redirect('/login')
+  }
+
+  return (
+    <div>
+      <Heading level={1}>My account</Heading>
+    </div>
+  )
+}
