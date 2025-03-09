6949f92ca9690ca124bed8f04f5e50634a308afa
Connor van Spronssen <info@dobefu.com>
2025-03-09 15:06:06 +0100

    feat: Remove the server-only import from the validateSession function

---
+++ src/utils/validate-session.ts
@@ -0,0 +1,65 @@
+import { cookies } from 'next/headers'
+import { getApiEndpoint } from './get-api-endpoint'
+import { getQueryClient } from './get-query-client'
+
+const errResponse = { isAnonymous: true }
+
+async function validateSession(): Promise<{ isAnonymous: boolean }> {
+  const queryClient = getQueryClient(0)
+  let validateResponse: Response = new Response()
+  let isQuerySuccessful
+
+  const apiEndpoint = getApiEndpoint()
+
+  if (!apiEndpoint) {
+    return errResponse
+  }
+
+  const cookieStore = await cookies()
+  const token = cookieStore.get('session')
+
+  if (!token) {
+    return errResponse
+  }
+
+  try {
+    validateResponse = await queryClient.fetchQuery({
+      queryKey: [apiEndpoint, token.value],
+      queryFn: async () => {
+        const formData = new FormData()
+        formData.append('session_token', token.value)
+
+        const response = await fetch(`${apiEndpoint}/validate-session`, {
+          method: 'POST',
+          body: formData,
+        })
+
+        if (!response.ok) {
+          const errJson = await response.json()
+
+          if (errJson && 'error' in errJson) {
+            throw new Error(errJson.error)
+          }
+
+          throw new Error(`response returned status code ${response.status}`)
+        }
+
+        return response
+      },
+    })
+
+    isQuerySuccessful = true
+  } catch (e) {
+    console.error(`Login failed: ${e}`)
+  }
+
+  if (!isQuerySuccessful) {
+    return { isAnonymous: true }
+  }
+
+  console.log(await validateResponse.json())
+
+  return { isAnonymous: true }
+}
+
+export { validateSession }
