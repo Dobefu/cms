70a9631b4ddaa0ccc972b2e74b99cf939299e7a8
Connor van Spronssen <info@dobefu.com>
2025-03-09 17:11:01 +0100

    feat: Add logic to renew the session token after five minutes

--- api/cmd/server/routes/v1/validate-session.go
+++ api/cmd/server/routes/v1/validate-session.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Dobefu/cms/api/cmd/database"
 	"github.com/Dobefu/cms/api/cmd/logger"
@@ -21,12 +22,14 @@ func ValidateSession(w http.ResponseWriter, r *http.Request) {
 	}
 
 	row := database.DB.QueryRow(
-		"SELECT token FROM sessions WHERE token = $1",
+		"SELECT user_id, token, updated_at FROM sessions WHERE token = $1",
 		oldToken,
 	)
 
+	var userId int
 	var token string
-	err := row.Scan(&token)
+	var lastUpdated time.Time
+	err := row.Scan(&userId, &token, &lastUpdated)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -35,7 +38,37 @@ func ValidateSession(w http.ResponseWriter, r *http.Request) {
 			logger.Error(err.Error())
 			utils.PrintError(w, user.ErrUnexpected, true)
 		}
+
 		return
+	}
+
+	tokenAge := time.Now().Unix() - lastUpdated.Unix()
+
+	if tokenAge > 360 {
+		newToken, err := user.UpdateSessionToken(userId)
+
+		if err != nil {
+			logger.Error(err.Error())
+			utils.PrintError(w, user.ErrUnexpected, true)
+
+			return
+		}
+
+		_, err = database.DB.Exec(
+			"UPDATE sessions SET token = $1, updated_at = $2 WHERE token = $3",
+			newToken,
+			time.Now(),
+			oldToken,
+		)
+
+		if err != nil {
+			logger.Error(err.Error())
+			utils.PrintError(w, user.ErrUnexpected, true)
+
+			return
+		}
+
+		token = newToken
 	}
 
 	fmt.Fprintf(w, `{"data":{"token":"%s"},"error":null}`, token)
