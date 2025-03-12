94250ea9a96772b85775ce22db02c5fbede9e52a
Connor van Spronssen <info@dobefu.com>
2025-03-12 17:07:18 +0100

    feat: Add more fields to the UserData struct

--- api/cmd/user/structs/user-data.go
+++ api/cmd/user/structs/user-data.go
@@ -1,5 +1,11 @@
 package structs
 
 type UserData struct {
-	Username string `json:"username"`
+	Id        int    `json:"id"`
+	Username  string `json:"username"`
+	Email     string `json:"email"`
+	Status    string `json:"status"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	LastLogin string `json:"last_login"`
 }
