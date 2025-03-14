6a1fc1edbcfe65a17dc4b1921b63b73ef3899aa6
Connor van Spronssen <info@dobefu.com>
2025-03-14 14:09:17 +0100

    feat: Add an icon to the dashboard link in the sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,10 +1,13 @@
+import iconDashboard from '@iconify/icons-mdi/view-dashboard'
+import { Icon, type IconifyIcon } from '@iconify/react/dist/iconify.js'
 import Link from 'next/link'
 
 export default function Sidebar() {
-  const links = [
+  const links: { title: string; href: string; icon: IconifyIcon | string }[] = [
     {
       title: 'Dashboard',
       href: '/dashboard',
+      icon: iconDashboard,
     },
   ]
 
@@ -12,10 +15,12 @@ export default function Sidebar() {
     <nav className="flex max-w-xs flex-1 flex-col py-4">
       {links.map((link) => (
         <Link
-          className="me-4 rounded-e-full p-4 font-medium transition-colors hover:bg-zinc-100 dark:hover:bg-zinc-700"
+          className="me-4 flex items-center gap-2 rounded-e-full p-4 font-medium transition-colors hover:bg-zinc-100 dark:hover:bg-zinc-700"
           href={link.href}
           key={link.href}
         >
+          <Icon className="size-8" icon={link.icon} ssr />
+
           {link.title}
         </Link>
       ))}
