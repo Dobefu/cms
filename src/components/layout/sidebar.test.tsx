7aa9377035e6bd4b3881bcec4aeaab600e368ae5
Connor van Spronssen <info@dobefu.com>
2025-03-14 13:55:28 +0100

    test: Add a test for the sidebar component

---
+++ src/components/layout/sidebar.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import Sidebar from './sidebar'
+
+describe('sidebar', () => {
+  it('renders normally', () => {
+    expect.hasAssertions()
+
+    render(<Sidebar />)
+
+    expect(screen.getByRole('navigation')).toBeDefined()
+  })
+})
