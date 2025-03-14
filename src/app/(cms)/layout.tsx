b9d9628f9ccd6cdd28f16ecba2eb01f82ce10e9f
Connor van Spronssen <info@dobefu.com>
2025-03-14 13:33:01 +0100

    feat: Add a placeholder sidebar component

--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -1,11 +1,17 @@
+import Sidebar from '@/components/layout/sidebar'
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return (
-    <main className="flex flex-1 flex-col gap-4 px-4" id="main-content">
-      {children}
-    </main>
+    <div className="flex flex-1">
+      <Sidebar />
+
+      <main className="flex flex-1 flex-col gap-4 px-4" id="main-content">
+        {children}
+      </main>
+    </div>
   )
 }
---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,3 @@
+export default function Sidebar() {
+  return <div className="max-w-xs flex-1">SIDEBAR</div>
+}
