0d98403c1688f29cfd238692e68fc095addbab5c
Connor van Spronssen <info@dobefu.com>
2025-03-15 13:43:01 +0100

    feat: Add some length constraints to fields in the "users" table

--- api/cmd/migrate_db/migrations/000001_create_users_table.up.sql
+++ api/cmd/migrate_db/migrations/000001_create_users_table.up.sql
@@ -1,7 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
 CREATE TABLE IF NOT EXISTS users(
   id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
-  username varchar(255) NOT NULL UNIQUE,
-  email varchar(254) NOT NULL UNIQUE,
+  username citext NOT NULL UNIQUE CONSTRAINT username_length CHECK (CHAR_LENGTH(username) <= 64),
+  email citext NOT NULL UNIQUE CONSTRAINT email_length CHECK (CHAR_LENGTH(email) <= 254),
   password varchar(255) NOT NULL,
   status boolean NOT NULL,
   created_at timestamp without time zone NOT NULL DEFAULT NOW(),
