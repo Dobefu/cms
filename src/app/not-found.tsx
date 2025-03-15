a4cf0e9a0bba96b545117ddfb8227659a20ce73a
Connor van Spronssen <info@dobefu.com>
2025-03-15 11:25:53 +0100

    fix: Re-add the footer to the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,20 @@
 import Heading from '@/components/elements/heading'
+import Footer from '@/components/layout/footer'
 
 export default function NotFound() {
   return (
-    <main
-      className="flex flex-1 flex-col gap-4 bg-zinc-100 p-4 shadow-inner dark:bg-zinc-700"
-      id="main-content"
-    >
-      <div className="flex flex-col gap-4">
-        <Heading level={1}>404</Heading>
-        <p>This page could not be found.</p>
-      </div>
-    </main>
+    <>
+      <main
+        className="flex flex-1 flex-col gap-4 bg-zinc-100 p-4 shadow-inner dark:bg-zinc-700"
+        id="main-content"
+      >
+        <div className="flex flex-col gap-4">
+          <Heading level={1}>404</Heading>
+          <p>This page could not be found.</p>
+        </div>
+      </main>
+
+      <Footer />
+    </>
   )
 }
