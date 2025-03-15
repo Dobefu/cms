77c2bbc500d74bd6fd87b8cb6f7fb06eb986c981
Connor van Spronssen <info@dobefu.com>
2025-03-15 13:13:28 +0100

    test: Add an additional test to the sidebar component

--- src/components/layout/sidebar.test.tsx
+++ src/components/layout/sidebar.test.tsx
@@ -1,13 +1,39 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import Sidebar from './sidebar'
 
 describe('sidebar', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   it('renders normally', () => {
     expect.hasAssertions()
 
     render(<Sidebar isCollapsedInitial={false} />)
 
     expect(screen.getByRole('navigation')).toBeDefined()
+  })
+
+  it('can be toggled', () => {
+    expect.hasAssertions()
+
+    render(<Sidebar isCollapsedInitial={false} />)
+
+    expect(
+      screen.queryByRole('navigation')?.getAttribute('data-collapsed'),
+    ).toBeNull()
+
+    fireEvent.click(screen.getByRole<HTMLInputElement>('button'))
+
+    expect(
+      screen.queryByRole('navigation')?.getAttribute('data-collapsed'),
+    ).toBe('true')
+
+    fireEvent.click(screen.getByRole<HTMLInputElement>('button'))
+
+    expect(
+      screen.queryByRole('navigation')?.getAttribute('data-collapsed'),
+    ).toBeNull()
   })
 })
