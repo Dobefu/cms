bb81443588dc0082dee31cb9864b0d9624d8b599
Connor van Spronssen <info@dobefu.com>
2025-03-15 13:31:54 +0100

    feat: Add logic to clear sessions of a user when the user gets deleted

--- api/cmd/migrate_db/migrations/000002_create_sessions_table.up.sql
+++ api/cmd/migrate_db/migrations/000002_create_sessions_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS sessions(
   user_id bigint PRIMARY KEY UNIQUE NOT NULL,
   token varchar(255) NOT NULL,
   updated_at timestamp without time zone NOT NULL DEFAULT NOW(),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX ON sessions(user_id);
