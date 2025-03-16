be9bbb6d4c8da32f1963a6de545ca3c49d7465ce
Connor van Spronssen <info@dobefu.com>
2025-03-16 15:19:45 +0100

    test: Redo the tests for the refactored API utility functions

--- src/utils/get-user-data.test.ts
+++ src/utils/get-user-data.test.ts
@@ -1,92 +1,25 @@
-import { cookies } from 'next/headers'
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { getUserData } from './get-user-data'
 
 describe('getUserData', () => {
-  beforeEach(() => {
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({}),
-      ok: false,
-      status: 422,
-    })
-  })
-
   afterEach(async () => {
-    process.env.API_ENDPOINT = 'http://api-endpoint'
     vi.restoreAllMocks()
-
-    const cookieStore = await cookies()
-    cookieStore.delete('session')
   })
 
-  it('returns early when the API endpoint is missing', async () => {
+  it('fetches the user data', async () => {
     expect.hasAssertions()
 
-    delete process.env.API_ENDPOINT
-
-    const { data, error } = await getUserData()
-
-    expect(error).toStrictEqual(new Error('API_ENDPOINT is not set'))
-    expect(data).toBeUndefined()
-  })
-
-  it('returns early when the session token is missing', async () => {
-    expect.hasAssertions()
-
-    const { data, error } = await getUserData()
-
-    expect(error).toStrictEqual(new Error('No session token cookie'))
-    expect(data).toBeUndefined()
-  })
-
-  it('returns a JSON error when the fetch call fails', async () => {
-    expect.hasAssertions()
-
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
-
-    const { data, error } = await getUserData()
-
-    expect(error).toStrictEqual(new Error('Getting user data failed'))
-    expect(data).toBeUndefined()
-  })
-
-  it('returns early when a failed response has an error object', async () => {
-    expect.hasAssertions()
-
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({ data: null, error: 'test error' }),
-      ok: false,
-      status: 422,
+    vi.mock('@/utils/fetch-api-data', async () => {
+      return {
+        fetchApiData: () => {
+          return { data: {}, error: undefined }
+        },
+      }
     })
 
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
-
     const { data, error } = await getUserData()
 
-    expect(error).toStrictEqual(new Error('Getting user data failed'))
-    expect(data).toBeUndefined()
-  })
-
-  it('returns a response when successful', async () => {
-    expect.hasAssertions()
-
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({ data: { user: {} }, error: null }),
-      ok: true,
-      status: 200,
-    })
-
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
-
-    const { data, error } = await getUserData()
-
-    expect(error).toBeNull()
-    expect(data).toStrictEqual({ user: {} })
+    expect(error).toBeUndefined()
+    expect(data).toStrictEqual({})
   })
 })
--- src/utils/validate-session.test.ts
+++ src/utils/validate-session.test.ts
@@ -1,97 +1,25 @@
-import { cookies } from 'next/headers'
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { validateSession } from './validate-session'
 
-describe('validateSession', () => {
-  beforeEach(() => {
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({}),
-      ok: false,
-      status: 422,
-    })
-  })
-
+describe('getUserData', () => {
   afterEach(async () => {
-    process.env.API_ENDPOINT = 'http://api-endpoint'
     vi.restoreAllMocks()
-
-    const cookieStore = await cookies()
-    cookieStore.delete('session')
   })
 
-  it('returns early when the API endpoint is missing', async () => {
+  it('validates the user session', async () => {
     expect.hasAssertions()
 
-    delete process.env.API_ENDPOINT
-
-    const response = await validateSession()
-
-    expect(response).toStrictEqual({ isAnonymous: true })
-  })
-
-  it('returns a JSON error when the fetch call fails', async () => {
-    expect.hasAssertions()
-
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
-
-    const response = await validateSession()
-
-    expect(response).toStrictEqual({ isAnonymous: true })
-  })
-
-  it('returns early when a failed response has an error object', async () => {
-    expect.hasAssertions()
-
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({ data: null, error: 'test error' }),
-      ok: false,
-      status: 422,
+    vi.mock('@/utils/fetch-api-data', async () => {
+      return {
+        fetchApiData: () => {
+          return { data: {}, error: undefined }
+        },
+      }
     })
 
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
+    const { isAnonymous, token } = await validateSession()
 
-    const response = await validateSession()
-
-    expect(response).toStrictEqual({ isAnonymous: true })
-  })
-
-  it('returns early when the token cannot be found in the response', async () => {
-    expect.hasAssertions()
-
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({ data: null, error: null }),
-      ok: true,
-      status: 200,
-    })
-
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
-
-    const response = await validateSession()
-
-    expect(response).toStrictEqual({ isAnonymous: true })
-  })
-
-  it('returns a response when successful', async () => {
-    expect.hasAssertions()
-
-    vi.spyOn(global, 'fetch').mockResolvedValue({
-      ...new Response(),
-      json: () => Promise.resolve({ data: { token: 'test' }, error: null }),
-      ok: true,
-      status: 200,
-    })
-
-    const cookieStore = await cookies()
-    cookieStore.set({ name: 'session', value: 'test' })
-
-    const response = await validateSession()
-
-    expect(response).toStrictEqual({ isAnonymous: false, token: 'test' })
+    expect(isAnonymous).toBe(true)
+    expect(token).toBeUndefined()
   })
 })
