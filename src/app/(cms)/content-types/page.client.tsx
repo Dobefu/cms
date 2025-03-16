d5787e7f3a20e00e84a893a126e87ae80180d783
Connor van Spronssen <info@dobefu.com>
2025-03-16 19:43:53 +0100

    feat: Add a fallback text when there are no content types

--- src/app/(cms)/content-types/page.client.tsx
+++ src/app/(cms)/content-types/page.client.tsx
@@ -20,35 +20,41 @@ export default function Client({ contentTypes }: Props) {
         </tr>
       </thead>
       <tbody>
-        {contentTypes
-          ? contentTypes.map((contentType) => (
-              <tr key={contentType.id}>
-                <td className="p-1">{contentType.id}</td>
-                <td className="w-full p-1">{contentType.title}</td>
-                <td className="flex gap-1 p-1">
-                  <Link
-                    className="btn"
-                    href={`/content-types/edit/${contentType.id}`}
-                  >
-                    Edit
-                  </Link>
+        {contentTypes ? (
+          contentTypes.map((contentType) => (
+            <tr key={contentType.id}>
+              <td className="p-1">{contentType.id}</td>
+              <td className="w-full p-1">{contentType.title}</td>
+              <td className="flex gap-1 p-1">
+                <Link
+                  className="btn"
+                  href={`/content-types/edit/${contentType.id}`}
+                >
+                  Edit
+                </Link>
 
-                  <button
-                    className="btn btn--danger"
-                    /* v8 ignore start */
-                    // eslint-disable-next-line react/jsx-no-bind
-                    onClick={async () => {
-                      await deleteContentType(contentType.id)
-                      redirect('')
-                    }}
-                    /* v8 ignore stop */
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))
-          : undefined}
+                <button
+                  className="btn btn--danger"
+                  /* v8 ignore start */
+                  // eslint-disable-next-line react/jsx-no-bind
+                  onClick={async () => {
+                    await deleteContentType(contentType.id)
+                    redirect('')
+                  }}
+                  /* v8 ignore stop */
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td className="p-8 text-center" colSpan={3}>
+              There are no content types yet
+            </td>
+          </tr>
+        )}
       </tbody>
     </table>
   )
