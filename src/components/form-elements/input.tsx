f3369f73012437abd1deedfecac73424f1d4b01d
Connor van Spronssen <info@dobefu.com>
2025-03-16 20:41:21 +0100

    feat: Make it possible to add an icon to an input

--- src/components/form-elements/input.tsx
+++ src/components/form-elements/input.tsx
@@ -1,26 +1,37 @@
 import { cn } from '@/utils/cn'
+import { Icon, IconifyIcon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
-    let typeClasses = ''
+type InputProps = React.ComponentProps<'input'> &
+  Readonly<{
+    icon?: IconifyIcon | string
+  }>
 
-    switch (type) {
-      case 'button':
-      case 'submit':
-        typeClasses = 'btn btn--primary'
-        break
-      default:
-        typeClasses =
-          'dark:border-zinc-600 dark:bg-zinc-950 border border-zinc-200'
-        break
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, icon, type, ...props }, ref) => {
+    if ((type === 'button' || type === 'submit') && icon) {
+      return (
+        <div className="relative">
+          <Icon
+            className="pointer-events-none absolute start-5 top-3.5 size-4 shrink-0"
+            icon={icon}
+            ssr
+          />
+
+          <input
+            className={cn('btn btn--primary ps-11', className)}
+            ref={ref}
+            type={type}
+            {...props}
+          />
+        </div>
+      )
     }
 
     return (
       <input
         className={cn(
-          'rounded-lg px-5 py-2.5 shadow-xs disabled:opacity-75',
-          typeClasses,
+          'rounded-lg border border-zinc-200 px-3 py-2.5 shadow-xs disabled:opacity-75 dark:border-zinc-600 dark:bg-zinc-950',
           className,
         )}
         ref={ref}
