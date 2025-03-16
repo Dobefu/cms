d0c3b038110051ddd973a548c03b9b5078f60eae
Connor van Spronssen <info@dobefu.com>
2025-03-16 13:20:02 +0100

    feat: Add logic to create the new content type

--- api/cmd/content/update-content-type.go
+++ api/cmd/content/update-content-type.go
@@ -2,11 +2,31 @@ package content
 
 import (
 	"errors"
+
+	"github.com/Dobefu/cms/api/cmd/database"
+	"github.com/Dobefu/cms/api/cmd/user"
 )
 
 func UpdateContentType(userId int, title string) (id int, err error) {
 	if title == "" {
 		return 0, errors.New("Missing title")
+	}
+
+	rows, err := database.DB.Query(
+		`INSERT INTO content_types (title, author_id) VALUES ($1, $2) RETURNING id`,
+		title,
+		userId,
+	)
+
+	if err != nil {
+		return 0, user.ErrUnexpected
+	}
+
+	rows.Next()
+	err = rows.Scan(&id)
+
+	if err != nil {
+		return 0, user.ErrUnexpected
 	}
 
 	return 1, nil
