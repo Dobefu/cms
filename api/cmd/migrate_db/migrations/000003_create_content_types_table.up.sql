40659095dfc16a2cfae30ef18d79b5eebf2079b9
Connor van Spronssen <info@dobefu.com>
2025-03-16 13:18:57 +0100

    fix: Remove the unique constraint from the author_id column

--- api/cmd/migrate_db/migrations/000003_create_content_types_table.up.sql
+++ api/cmd/migrate_db/migrations/000003_create_content_types_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS content_types(
   id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
   title varchar(255) NOT NULL,
-  author_id bigint UNIQUE NOT NULL,
+  author_id bigint NOT NULL,
   created_at timestamp without time zone NOT NULL DEFAULT NOW(),
   updated_at timestamp without time zone NOT NULL DEFAULT NOW(),
   FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-CREATE UNIQUE INDEX ON content_types(id, author_id);
+CREATE UNIQUE INDEX ON content_types(id);
+CREATE INDEX ON content_types(author_id);
