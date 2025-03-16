95dc2c388d276a451b1ce36b367f9198fc32f297
Connor van Spronssen <info@dobefu.com>
2025-03-16 20:05:32 +0100

    fix: Resolve inconsistent padding on button

--- src/components/form-elements/input.tsx
+++ src/components/form-elements/input.tsx
@@ -19,7 +19,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
     return (
       <input
         className={cn(
-          'rounded-lg px-3 py-2.5 shadow-xs disabled:opacity-75',
+          'rounded-lg px-5 py-2.5 shadow-xs disabled:opacity-75',
           typeClasses,
           className,
         )}
