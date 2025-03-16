ccf9ad866f8bddc822026d7edbc065483ef0a5da
Connor van Spronssen <info@dobefu.com>
2025-03-16 16:23:10 +0100

    feat: Add a utility function to get a single content type

---
+++ src/utils/get-content-type.ts
@@ -0,0 +1,15 @@
+import { ContentType } from '@/types/content-type'
+import { fetchApiData } from './fetch-api-data'
+
+async function getContentType(id: number): Promise<{
+  data?: { content_type: ContentType }
+  error?: Error
+}> {
+  return await fetchApiData<{ content_type: ContentType }>({
+    method: 'GET',
+    path: `/content-type/${id}`,
+    queryKeyParts: [id],
+  })
+}
+
+export { getContentType }
