cbf7c058b1a1b17c0fb617e980cd3a402a849b8d
Connor van Spronssen <info@dobefu.com>
2025-03-20 11:58:52 +0100

    test: Clean up the heading tests

--- src/components/elements/heading.test.tsx
+++ src/components/elements/heading.test.tsx
@@ -3,52 +3,16 @@ import { describe, expect, it } from 'vitest'
 import Heading from './heading'
 
 describe('heading', () => {
-  it('renders normally (h1)', () => {
+  it('renders normally', () => {
     expect.hasAssertions()
 
-    render(<Heading level={1}>TITLE</Heading>)
+    for (let i = 0; i < 6; i++) {
+      render(<Heading level={i + 1}>TITLE</Heading>)
+    }
 
-    expect(screen.getByRole('heading').className).toContain('text-3xl')
-  })
-
-  it('renders normally (h2)', () => {
-    expect.hasAssertions()
-
-    render(<Heading level={2}>TITLE</Heading>)
-
-    expect(screen.getByRole('heading').className).toContain('text-2xl')
-  })
-
-  it('renders normally (h3)', () => {
-    expect.hasAssertions()
-
-    render(<Heading level={3}>TITLE</Heading>)
-
-    expect(screen.getByRole('heading').className).toContain('text-xl')
-  })
-
-  it('renders normally (h4)', () => {
-    expect.hasAssertions()
-
-    render(<Heading level={4}>TITLE</Heading>)
-
-    expect(screen.getByRole('heading').className).toContain('text-lg')
-  })
-
-  it('renders normally (h5)', () => {
-    expect.hasAssertions()
-
-    render(<Heading level={5}>TITLE</Heading>)
-
-    expect(screen.getByRole('heading').className).toContain('text-md')
-  })
-
-  it('renders normally (h6)', () => {
-    expect.hasAssertions()
-
-    render(<Heading level={6}>TITLE</Heading>)
-
-    expect(screen.getByRole('heading').className).toContain('text-sm')
+    screen.getAllByRole('heading').forEach((heading, index) => {
+      expect(heading.tagName).toBe(`H${index + 1}`)
+    })
   })
 
   it('does not render on invalid levels', () => {
