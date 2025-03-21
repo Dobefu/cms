3fe3c4a24479f51e1da7016bab7f80dd2883efdd
Connor van Spronssen <info@dobefu.com>
2025-03-21 14:24:00 +0100

    feat: Add a modal component

---
+++ src/components/layout/modal.client.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import iconClose from '@iconify/icons-mdi/close'
+import { Icon } from '@iconify/react/dist/iconify.js'
+import { useRouter } from 'next/navigation'
+import { type ComponentRef, useCallback, useEffect, useRef } from 'react'
+import { createPortal } from 'react-dom'
+
+export type Props = Readonly<{
+  children: React.ReactNode
+}>
+
+export default function Modal({ children }: Props) {
+  const router = useRouter()
+  const dialogRef = useRef<ComponentRef<'dialog'>>(null)
+
+  useEffect(() => {
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.show()
+    }
+  }, [])
+
+  const closeModal = useCallback(() => {
+    router.back()
+  }, [router])
+
+  return createPortal(
+    <>
+      <div
+        className="fixed inset-0 z-50 bg-black/20 backdrop-blur-sm"
+        onClick={closeModal}
+      />
+
+      <dialog
+        className="start-1/2 top-1/2 z-50 -translate-1/2 rounded-lg bg-white p-6 shadow-sm dark:bg-zinc-900"
+        onClose={closeModal}
+        ref={dialogRef}
+      >
+        <div className="-m-6 mb-0 p-2">
+          <button className="btn ms-auto rounded-full p-2" onClick={closeModal}>
+            <Icon className="size-4" icon={iconClose} ssr />
+          </button>
+        </div>
+
+        {children}
+      </dialog>
+    </>,
+    document.getElementById('modal-root')!,
+  )
+}
