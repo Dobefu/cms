77464ca2015527c6a9e10fedabb804304658793e
Connor van Spronssen <info@dobefu.com>
2025-03-22 14:10:27 +0100

    feat: Add a foreign key constraint on the content type ID to the content table

--- api/cmd/migrate_db/migrations/000004_create_content_table.up.sql
+++ api/cmd/migrate_db/migrations/000004_create_content_table.up.sql
@@ -1,10 +1,12 @@
 CREATE TABLE IF NOT EXISTS content(
   id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
+  content_type bigint,
   title varchar(255) NOT NULL,
   author_id bigint NOT NULL,
   created_at timestamp without time zone NOT NULL DEFAULT NOW(),
   updated_at timestamp without time zone NOT NULL DEFAULT NOW(),
-  FOREIGN KEY (author_id) REFERENCES users(id)
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  CONSTRAINT content_type_id FOREIGN KEY (content_type) REFERENCES content_types(id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX ON content(id);
