708d5606c10fcf2b5fc25b9efd0951951b59bb96
Connor van Spronssen <info@dobefu.com>
2025-03-23 12:57:28 +0100

    test: Add tests to the content route

--- src/app/(cms)/content/page.test.tsx
+++ src/app/(cms)/content/page.test.tsx
@@ -1,13 +1,55 @@
-import { render } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import { render, screen, waitFor } from '@testing-library/react'
+import { cookies } from 'next/headers'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import Content from './page'
 
+const cookieStore = await cookies()
+
 describe('content', () => {
-  it('renders normally', () => {
+  beforeEach(() => {
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      ...new Response(),
+      json: () =>
+        Promise.resolve({
+          data: {
+            token: '',
+            content: [{ id: 1, title: 'Content' }],
+          },
+        }),
+      ok: true,
+      status: 200,
+    })
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+
+    cookieStore.delete('session')
+  })
+
+  it('renders normally', async () => {
     expect.hasAssertions()
 
     render(<Content />)
 
+    await waitFor(() => {
+      expect(screen.getByText('There is no content yet')).toBeDefined()
+    })
+
     expect(screen).toBeDefined()
+  })
+
+  it('can delete content', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    render(<Content />)
+
+    expect(screen.getByText('Loading...')).toBeDefined()
+
+    await waitFor(() => {
+      expect(screen.getByText('Delete')).toBeDefined()
+    })
   })
 })
