08a4e4e5661a10fc705968d0dd0440905e45f40e
Connor van Spronssen <info@dobefu.com>
2025-03-23 12:14:58 +0100

    test: Add tests to the CreateContent functions

---
+++ api/cmd/content/create-content_test.go
@@ -0,0 +1,67 @@
+package content
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/Dobefu/cms/api/cmd/database"
+	"github.com/Dobefu/cms/api/cmd/user"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupCreateContentTests(t *testing.T) (mock sqlmock.Sqlmock, cleanup func()) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+
+	oldDb := database.DB
+	database.DB = db
+
+	return mock, func() {
+		db.Close()
+		database.DB = oldDb
+	}
+}
+
+func TestCreateContentErrMissingTitle(t *testing.T) {
+	_, cleanup := setupCreateContentTests(t)
+	defer cleanup()
+
+	id, err := CreateContent(1, 1, "")
+	assert.EqualError(t, err, "Missing title")
+	assert.Equal(t, 0, id)
+}
+
+func TestCreateContentErrInsert(t *testing.T) {
+	_, cleanup := setupCreateContentTests(t)
+	defer cleanup()
+
+	id, err := CreateContent(1, 1, "Title")
+	assert.EqualError(t, err, user.ErrUnexpected.Error())
+	assert.Equal(t, 0, id)
+}
+
+func TestCreateContentErrScan(t *testing.T) {
+	mock, cleanup := setupCreateContentTests(t)
+	defer cleanup()
+
+	mock.ExpectQuery("INSERT INTO content (.+) VALUES (.+) RETURNING id").WillReturnRows(
+		sqlmock.NewRows([]string{"id"}).AddRow("bogus"),
+	)
+
+	id, err := CreateContent(1, 1, "Title")
+	assert.EqualError(t, err, user.ErrUnexpected.Error())
+	assert.Equal(t, 0, id)
+}
+
+func TestCreateContentSuccess(t *testing.T) {
+	mock, cleanup := setupCreateContentTests(t)
+	defer cleanup()
+
+	mock.ExpectQuery("INSERT INTO content (.+) VALUES (.+) RETURNING id").WillReturnRows(
+		sqlmock.NewRows([]string{"id"}).AddRow(1),
+	)
+
+	id, err := CreateContent(1, 1, "Title")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, id)
+}
---
+++ api/cmd/server/routes/v1/create-content_test.go
@@ -0,0 +1,113 @@
+package v1
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/Dobefu/cms/api/cmd/content"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupCreateContentTests() (rr *httptest.ResponseRecorder, cleanup func()) {
+	rr = httptest.NewRecorder()
+
+	userValidateSession = func(oldToken string, refresh bool) (newToken string, userId int, err error) {
+		return "", 1, nil
+	}
+
+	contentCreateContent = func(userId int, contentType int, title string) (id int, err error) {
+		return 1, nil
+	}
+
+	return rr, func() {
+		contentCreateContent = content.CreateContent
+	}
+}
+
+func TestCreateContentErrInvalidSessionToken(t *testing.T) {
+	rr, cleanup := setupCreateContentTests()
+	defer cleanup()
+
+	userValidateSession = func(oldToken string, refresh bool) (newToken string, userId int, err error) {
+		return "", 0, assert.AnError
+	}
+
+	req, err := http.NewRequest("PUT", "", nil)
+	req.Header.Add("Session-Token", "bogus")
+	assert.NoError(t, err)
+
+	CreateContent(rr, req)
+	assert.JSONEq(t, fmt.Sprintf(`{"data": null, "error": "%s"}`, assert.AnError), rr.Body.String())
+}
+
+func TestCreateContentErrMissingSessionToken(t *testing.T) {
+	rr, cleanup := setupCreateContentTests()
+	defer cleanup()
+
+	req, err := http.NewRequest("PUT", "", nil)
+	assert.NoError(t, err)
+
+	CreateContent(rr, req)
+	assert.JSONEq(t, fmt.Sprintf(`{"data": null, "error": "%s"}`, "Missing session_token"), rr.Body.String())
+}
+
+func TestCreateContentErrMissingParams(t *testing.T) {
+	rr, cleanup := setupCreateContentTests()
+	defer cleanup()
+
+	req, err := http.NewRequest("PUT", "", nil)
+	req.Header.Add("Session-Token", "test")
+	assert.NoError(t, err)
+
+	CreateContent(rr, req)
+	assert.JSONEq(t, fmt.Sprintf(`{"data": null, "error": "%s"}`, "Missing or invalid content type ID"), rr.Body.String())
+}
+
+func TestCreateContentErrMissingTitle(t *testing.T) {
+	rr, cleanup := setupCreateContentTests()
+	defer cleanup()
+
+	req, err := http.NewRequest("PUT", "", strings.NewReader("content_type=1"))
+	req.Header.Add("Session-Token", "test")
+	assert.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	CreateContent(rr, req)
+	assert.JSONEq(t, fmt.Sprintf(`{"data": null, "error": "%s"}`, "Missing title"), rr.Body.String())
+}
+
+func TestCreateContentErrCreateContent(t *testing.T) {
+	rr, cleanup := setupCreateContentTests()
+	defer cleanup()
+
+	contentCreateContent = func(userId int, contentType int, title string) (id int, err error) {
+		return 0, assert.AnError
+	}
+
+	req, err := http.NewRequest("PUT", "", strings.NewReader("content_type=1&title=Title"))
+	req.Header.Add("Session-Token", "test")
+	assert.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	CreateContent(rr, req)
+	assert.JSONEq(t, fmt.Sprintf(`{"data": null, "error": "%s"}`, assert.AnError), rr.Body.String())
+}
+
+func TestCreateContentSuccess(t *testing.T) {
+	rr, cleanup := setupCreateContentTests()
+	defer cleanup()
+
+	req, err := http.NewRequest("PUT", "", strings.NewReader("content_type=1&title=Title"))
+	req.Header.Add("Session-Token", "test")
+	assert.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	CreateContent(rr, req)
+	assert.JSONEq(t, fmt.Sprintf(`{"data": {"id": %d}, "error": null}`, 1), rr.Body.String())
+}
