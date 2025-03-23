c5a2566457fe2ea7a24b585824d808ae2eb21ad6
Connor van Spronssen <info@dobefu.com>
2025-03-23 13:27:10 +0100

    feat: Add a submitContent form action

---
+++ src/actions/submit-content.ts
@@ -0,0 +1,79 @@
+'use server'
+
+import { fetchApiData } from '@/utils/fetch-api-data'
+import { getApiEndpoint } from '@/utils/get-api-endpoint'
+import { validateForm } from '@/utils/validate-form'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import * as v from 'valibot'
+
+export interface FormState {
+  id?: number
+  title: string
+  errors: {
+    title?: string[]
+    generic?: string[]
+  }
+}
+
+const ContentSchema = v.object({
+  title: v.pipe(
+    v.string('The title must be a string'),
+    v.nonEmpty('Please enter a title'),
+    v.minLength(3, 'Please enter a title of at least 3 characters'),
+  ),
+})
+
+export async function submitContent(
+  prevState: FormState,
+  formData: FormData,
+): Promise<FormState> {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('session')
+
+  if (!token) {
+    return prevState
+  }
+
+  const title = formData.get('title') as string
+
+  const { isValid, newState } = validateForm<typeof ContentSchema, FormState>(
+    ContentSchema,
+    prevState,
+    { title },
+  )
+
+  if (!isValid) {
+    return newState
+  }
+
+  const apiEndpoint = getApiEndpoint()
+
+  if (!apiEndpoint) {
+    return newState
+  }
+
+  const { data, error } = await fetchApiData<{ id: string }>({
+    path: prevState.id ? `/content/${prevState.id}` : '/content',
+    method: prevState.id ? 'POST' : 'PUT',
+    body: formData,
+    queryKeyParts: [title, prevState.id],
+  })
+
+  if (error) {
+    newState.errors.generic = ['Content submission failed']
+    console.error(`Content submission failed: ${error.message}`)
+    return newState
+  }
+
+  if (data?.id) {
+    if (prevState.id) {
+      return newState
+    }
+
+    redirect(`/content/edit/${data.id}`)
+  } else {
+    newState.errors.generic = ['Content submission failed']
+    return newState
+  }
+}
