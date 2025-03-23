2ba9b8221d8ba8dc9e366c14a6e6710442d32346
Connor van Spronssen <info@dobefu.com>
2025-03-23 13:27:23 +0100

    test: Add tests to the submitContent form action

---
+++ src/actions/submit-content.test.ts
@@ -0,0 +1,170 @@
+import { cookies } from 'next/headers'
+import * as navigation from 'next/navigation'
+import {
+  type MockInstance,
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from 'vitest'
+import { type FormState, submitContent } from './submit-content'
+
+let initialState: FormState
+let spy: MockInstance
+let formData: FormData
+const cookieStore = await cookies()
+
+describe('submitContent', () => {
+  const oldApiEndpoint = process.env.API_ENDPOINT
+
+  beforeEach(() => {
+    spy = vi.spyOn(navigation, 'redirect')
+    formData = new FormData()
+
+    initialState = {
+      title: '',
+      errors: {
+        title: undefined,
+        generic: undefined,
+      },
+    }
+
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      ...new Response(),
+      json: () => Promise.resolve({}),
+      ok: false,
+      status: 422,
+    })
+  })
+
+  afterEach(() => {
+    process.env.API_ENDPOINT = oldApiEndpoint
+    vi.restoreAllMocks()
+
+    cookieStore.delete('session')
+  })
+
+  it('returns early when the session token cookie is missing', async () => {
+    expect.hasAssertions()
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('returns early when the API endpoint is missing', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    delete process.env.API_ENDPOINT
+
+    formData.append('title', 'Title')
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('returns a JSON error when the fetch call fails', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    formData.append('title', 'Title')
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('returns early when there is missing data', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('returns early when a failed response has an error object', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      ...new Response(),
+      json: () => Promise.resolve({ data: null, error: 'test error' }),
+      ok: false,
+      status: 422,
+    })
+
+    formData.append('title', 'Title')
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('returns early when a response token is missing', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      ...new Response(),
+      json: () => Promise.resolve({ data: null, error: null }),
+      ok: true,
+      status: 200,
+    })
+
+    formData.append('title', 'Title')
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  it('redirects when creating new content', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      ...new Response(),
+      json: () => Promise.resolve({ data: { id: 1 }, error: null }),
+      ok: true,
+      status: 200,
+    })
+
+    formData.append('title', 'Title')
+
+    await expect(submitContent(initialState, formData)).rejects.toThrow(
+      'Mock redirect error',
+    )
+  })
+
+  it('does not redirect when updating content', async () => {
+    expect.hasAssertions()
+
+    cookieStore.set({ name: 'session', value: 'test' })
+
+    initialState.id = 1
+
+    vi.spyOn(global, 'fetch').mockResolvedValue({
+      ...new Response(),
+      json: () => Promise.resolve({ data: { id: 1 }, error: null }),
+      ok: true,
+      status: 200,
+    })
+
+    formData.append('title', 'Title')
+
+    await submitContent(initialState, formData)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+})
