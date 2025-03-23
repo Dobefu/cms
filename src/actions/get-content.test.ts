ae2e082a1e5d886d912e03e6a87e7abb9ac9b63a
Connor van Spronssen <info@dobefu.com>
2025-03-23 12:51:17 +0100

    test: Add tests to the getContent form action

---
+++ src/actions/get-content.test.ts
@@ -0,0 +1,25 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { getContent } from './get-content'
+
+describe('getContent', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('fetches the content', async () => {
+    expect.hasAssertions()
+
+    vi.mock('@/utils/fetch-api-data', () => {
+      return {
+        fetchApiData: () => {
+          return { data: {}, error: undefined }
+        },
+      }
+    })
+
+    const { data, error } = await getContent()
+
+    expect(error).toBeUndefined()
+    expect(data).toStrictEqual({})
+  })
+})
