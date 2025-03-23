b04ce7a9abdd38bbf3db6119d387ca476a0b6073
Connor van Spronssen <info@dobefu.com>
2025-03-23 15:13:11 +0100

    feat: Update the Content TypeScript Type

--- src/types/content.ts
+++ src/types/content.ts
@@ -1,6 +1,8 @@
+import { type ContentType } from './content-type'
+
 export type Content = {
   id: number
-  content_type: number
+  content_type: ContentType
   title: string
   created_at: string
   updated_at: string
