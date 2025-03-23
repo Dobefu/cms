6d4f1825b67bbe891f09a3fc51391746b425ab3b
Connor van Spronssen <info@dobefu.com>
2025-03-23 15:15:19 +0100

    test: Add tests to the content edit page

---
+++ src/app/(cms)/content/edit/[id]/page.test.tsx
@@ -0,0 +1,35 @@
+import { fetchApiData } from '@/utils/fetch-api-data'
+import { render } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
+import EditContentEntry from './page'
+
+describe('content/edit/[id]', () => {
+  vi.mock('@/utils/fetch-api-data', () => ({
+    fetchApiData: ({ path }: (typeof fetchApiData)['arguments']) => {
+      if (path === '/content/1') {
+        return {
+          data: { content: { id: 1, content_type: { id: 1 } } },
+          error: undefined,
+        }
+      }
+
+      return { data: undefined, error: new Error('') }
+    },
+  }))
+
+  it('redirects if a content does not exist', async () => {
+    expect.hasAssertions()
+
+    await expect(
+      EditContentEntry({ params: Promise.resolve({ id: 0 }) }),
+    ).rejects.toThrow('Mock notFound error')
+  })
+
+  it('renders normally', async () => {
+    expect.hasAssertions()
+
+    render(await EditContentEntry({ params: Promise.resolve({ id: 1 }) }))
+
+    expect(screen).toBeDefined()
+  })
+})
