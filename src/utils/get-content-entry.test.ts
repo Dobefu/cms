459165a35f0c4a941cbc95234e8a4a994c016b03
Connor van Spronssen <info@dobefu.com>
2025-03-23 14:31:08 +0100

    test: Add tests to the getContentEntry util

---
+++ src/utils/get-content-entry.test.ts
@@ -0,0 +1,25 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { getContentEntry } from './get-content-entry'
+
+describe('getContentEntry', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('fetches the content entry', async () => {
+    expect.hasAssertions()
+
+    vi.mock('@/utils/fetch-api-data', () => {
+      return {
+        fetchApiData: () => {
+          return { data: {}, error: undefined }
+        },
+      }
+    })
+
+    const { data, error } = await getContentEntry(1)
+
+    expect(error).toBeUndefined()
+    expect(data).toStrictEqual({})
+  })
+})
